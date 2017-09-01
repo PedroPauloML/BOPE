94f912b25b1130cf9af99732d19b834a63815355
Pedro Paulo Marques <pedropaulomarqz@gmail.com>
2017-09-01 18:39:39 +0000

    Correção do erro de viollação da chave primária.

--- app/models/macro_activity.rb
+++ app/models/macro_activity.rb
@@ -2,7 +2,7 @@ class MacroActivity < ApplicationRecord
 
   # Associations
   belongs_to :project
-  has_many :sub_macro_activities
+  has_many :sub_macro_activities, dependent: :destroy
 
   # Validations
   validates_presence_of :description, :project_id
